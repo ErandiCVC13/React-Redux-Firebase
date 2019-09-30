525e549c90269d9d2df70d2c106df8e033cd5a29
ErandiCVC13 <hgftdrd@hotmail.com>
2019-09-30 18:23:58 -0500

    First commit

--- marioplan/src/config/fbConfig.js
+++ marioplan/src/config/fbConfig.js
@@ -18,6 +18,6 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-firebase.firestore().settings({ timestampsInSnapshots: true });
+// firebase.firestore().settings({ timestampsInSnapshots: true });
 
 export default firebase;
